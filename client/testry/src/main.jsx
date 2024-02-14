1b88a39bec779f82848c88e8749902ae4f0f8855
unknown <heber01mm@gmail.com>
2024-02-14 20:52:02 -0300

    estructurando el cliente

---
+++ client/testry/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { testeando } from "./prueba";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <testeando/>
+  </React.StrictMode>
+);
---
+++ client/testry/src/prueba.jsx
@@ -0,0 +1,15 @@
+
+
+const string = "esto es un texto";
+const number = 12345;
+const array = ["Hola", "xd", "test"];
+const boolean = true;
+const funcion = () => 1 + 1;
+const objeto = { nombre: "testing", duracion: 9 };
+const fecha = new Date();
+
+export const testeando = () => {
+  return (
+ <h1>{string}</h1>
+  )
+};
